a36a55632aae69adcdd1df7059d2b02c25280455
Jaroslav2001 <67440530+Jaroslav2001@users.noreply.github.com>
2021-05-23 12:37:27 +0200

    user path

--- models/users.py
+++ models/users.py
@@ -8,6 +8,6 @@ class Users(ormar.Model):
         metadata = metadata
 
     id: int = ormar.Integer(primary_key=True)
-    name: str = ormar.String(max_length=100, unique=True)
-    password: str = ormar.String(max_length=500)
+    name: str = ormar.String(max_length=100, unique=True, nullable=False)
+    password: str = ormar.String(max_length=500, nullable=False)
     email: str = ormar.String(max_length=100)
